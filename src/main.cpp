d5b75c81813ffca1b68ba9051be376ed5413e3cd
musee-00 <gemu21ly@student.ju.se>
2025-09-18 09:26:07 +0200

    Test CI workflow: update output message

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
+    int a = 5, b = 3;
     std::cout << "Hello, BITE pipeline!" << std::endl;
+    std::cout << "Sum: " << (a + b) << std::endl;
     return 0;
 }
-
